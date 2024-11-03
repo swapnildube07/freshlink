eccb1d46281daf9e8cce718645bd73a7efdae2c2
Swapnil077 <swapnildube07@gmail.com>
2024-11-03 07:22:45 +0530

    Add new files

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid
-      ? await Firebase.initializeApp(
+  Platform.isAndroid?
+       await Firebase.initializeApp(
           options: const FirebaseOptions(
               apiKey: 'AIzaSyAF5KRZPnTUjPKjf_3M27BbJYw3g-M6I6w',
               appId: '1:810821731815:android:e4d328337a97a6a844e942',
@@ -16,6 +16,7 @@ void main() async {
               projectId: 'fresh-1bb19',
               storageBucket: 'fresh-1bb19.appspot.com'),
         )
+
       : await Firebase.initializeApp();
 
   runApp(const MyApp());
